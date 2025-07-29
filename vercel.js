6e5c1f0885f20db68e8d4912fbb61493c13ad06e
Sylvain Herbin <herbinsylvain@protonmail.com>
2025-07-29 16:17:47 +0200

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,9 @@
+{
+  "functions": {
+    "api/**/*.js": {
+      "runtime": "nodejs20.x"
+    }
+  },
+  "installCommand": "npm install",
+  "buildCommand": "echo 'No build step required for API functions'"
+}
